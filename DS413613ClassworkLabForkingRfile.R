2f7e4fb529e92ca7758884d7564b96f691ba83c2
ptlpavan <92137822+ptlpavan@users.noreply.github.com>
2024-03-26 18:23:52 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -6,10 +6,13 @@ mpg
 # My favorite color is blue
 # This is my coding that generates Boxplots for transmission types
 ggplot(data = mpg) +
-  geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
+  geom_boxplot(mapping = aes(x = drv, y = displ, color = "blue")) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
-  ylab("displacement") 
+  ylab("displacement") -> g1
+
+g1 + coord_flip()
+
   
 
 
